0321bd4795d920216117c6687b722d50c8e38d98
Agustin Linari <ALINARI@gire.com>
2018-05-19 16:00:31 -0300

    Proyecto Base

---
+++ server/src/main/java/com/gire/eval360/Eval360Application.java
@@ -0,0 +1,12 @@
+package com.gire.eval360;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Eval360Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Eval360Application.class, args);
+	}
+}
