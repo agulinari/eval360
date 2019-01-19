87cbbe659f69fd42323d1a465622c4aa876ec99c
Agustin Linari <ALINARI@gire.com>
2019-01-19 17:25:00 -0300

    Ejemplo de reporte con thymeleaf y flying saucer

---
+++ reports/src/main/java/com/gire/eval360/reports/configuration/ThymeleafConfiguration.java
@@ -0,0 +1,30 @@
+package com.gire.eval360.reports.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+
+@Configuration
+@EnableWebMvc
+public class ThymeleafConfiguration {
+
+    @Bean
+    public SpringTemplateEngine templateEngine() {
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.setTemplateResolver(thymeleafTemplateResolver());
+        return templateEngine;
+    }
+
+    @Bean
+    public SpringResourceTemplateResolver thymeleafTemplateResolver() {
+        SpringResourceTemplateResolver templateResolver 
+          = new SpringResourceTemplateResolver();
+        templateResolver.setPrefix("classpath:/templates/");
+        templateResolver.setSuffix(".html");
+        templateResolver.setTemplateMode("HTML5");
+        templateResolver.setCharacterEncoding("UTF-8");
+        return templateResolver;
+    }
+}
--- reports/src/main/java/com/gire/eval360/reports/controller/ReportController.java
+++ reports/src/main/java/com/gire/eval360/reports/controller/ReportController.java
@@ -1,10 +1,161 @@
 package com.gire.eval360.reports.controller;
 
+import static com.itextpdf.text.pdf.BaseFont.EMBEDDED;
+import static com.itextpdf.text.pdf.BaseFont.IDENTITY_H;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
+import java.nio.file.FileSystems;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+import org.w3c.tidy.Tidy;
+import org.xhtmlrenderer.pdf.ITextRenderer;
 
+import lombok.extern.slf4j.Slf4j;
+
+
+@Slf4j
 @RestController
 @RequestMapping("/reports")
 public class ReportController {
 
+    private static final String OUTPUT_FILE = "test.pdf";
+    private static final String UTF_8 = "UTF-8";
+    
+    @Autowired
+    TemplateEngine templateEngine;
+
+    @GetMapping
+    public void generatePdf(HttpServletResponse response) throws Exception {
+
+        // The data in our Thymeleaf templates is not hard-coded. Instead,
+        // we use placeholders in our templates. We fill these placeholders
+        // with actual data by passing in an object. In this example, we will
+        // write a letter to "John Doe".
+        //
+        // Note that we could also read this data from a JSON file, a database
+        // a web service or whatever.
+        Data data = exampleDataForJohnDoe();
+
+        Context context = new Context();
+        context.setVariable("data", data);
+
+        // Flying Saucer needs XHTML - not just normal HTML. To make our life
+        // easy, we use JTidy to convert the rendered Thymeleaf template to
+        // XHTML. Note that this might no work for very complicated HTML. But
+        // it's good enough for a simple letter.
+        String renderedHtmlContent = templateEngine.process("template", context);
+        String xHtml = convertToXhtml(renderedHtmlContent);
+
+        ITextRenderer renderer = new ITextRenderer();
+        renderer.getFontResolver().addFont("Code39.ttf", IDENTITY_H, EMBEDDED);
+
+        // FlyingSaucer has a working directory. If you run this test, the working directory
+        // will be the root folder of your project. However, all files (HTML, CSS, etc.) are
+        // located under "/src/test/resources". So we want to use this folder as the working
+        // directory.
+        String baseUrl = FileSystems
+                                .getDefault()
+                                .getPath("src", "main", "resources", "templates")
+                                .toUri()
+                                .toURL()
+                                .toString();
+        renderer.setDocumentFromString(xHtml, baseUrl);
+        renderer.layout();
+        
+        try {
+        // And finally, we create the PDF:
+       // OutputStream outputStream = new FileOutputStream(OUTPUT_FILE);
+        OutputStream outputStream = response.getOutputStream();
+        renderer.createPDF(outputStream);
+        response.setContentType("application/pdf");
+        response.flushBuffer();
+        } catch (IOException ex) {
+            log.info("Error writing file to output stream. ", ex);
+            throw new RuntimeException("IOError writing file to output stream");
+          }
+       // outputStream.close();
+        
+    }
+
+    private Data exampleDataForJohnDoe() {
+        Data data = new Data();
+        data.setFirstname("John");
+        data.setLastname("Doe");
+        data.setStreet("Example Street 1");
+        data.setZipCode("12345");
+        data.setCity("Example City");
+        return data;
+    }
+
+    static class Data {
+        private String firstname;
+        private String lastname;
+        private String street;
+        private String zipCode;
+        private String city;
+
+        public String getFirstname() {
+            return firstname;
+        }
+
+        public void setFirstname(String firstname) {
+            this.firstname = firstname;
+        }
+
+        public String getLastname() {
+            return lastname;
+        }
+
+        public void setLastname(String lastname) {
+            this.lastname = lastname;
+        }
+
+        public String getStreet() {
+            return street;
+        }
+
+        public void setStreet(String street) {
+            this.street = street;
+        }
+
+        public String getZipCode() {
+            return zipCode;
+        }
+
+        public void setZipCode(String zipCode) {
+            this.zipCode = zipCode;
+        }
+
+        public String getCity() {
+            return city;
+        }
+
+        public void setCity(String city) {
+            this.city = city;
+        }
+    }
+
+    private String convertToXhtml(String html) throws UnsupportedEncodingException {
+        Tidy tidy = new Tidy();
+        tidy.setInputEncoding(UTF_8);
+        tidy.setOutputEncoding(UTF_8);
+        tidy.setXHTML(true);
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(html.getBytes(UTF_8));
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        tidy.parseDOM(inputStream, outputStream);
+        return outputStream.toString(UTF_8);
+    }
+	
+	
 }
