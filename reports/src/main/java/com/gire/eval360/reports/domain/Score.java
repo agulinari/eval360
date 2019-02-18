133257b46ada4431ec5238a2a0ffc0ac3a962051
Agustin Linari <ALINARI@gire.com>
2019-02-17 23:25:45 -0300

    clases reporte

--- reports/src/main/java/com/gire/eval360/reports/domain/Score.java
+++ reports/src/main/java/com/gire/eval360/reports/domain/Score.java
@@ -2,6 +2,16 @@ package com.gire.eval360.reports.domain;
 
 public enum Score {
 	
-	SMALL, MEDIUM, LARGE;
+	SMALL("tdsmall"), MEDIUM("tdmedium"), LARGE("tdlarge");
+	
+	private final String cssClass;
+	
+	private Score(String cssClass) {
+		this.cssClass = cssClass;
+	}
+	
+	public String getCssClass() {
+		return cssClass;
+	}
 
 }
