57d66bfdfec4ffbd3d3e60ee1643895f5311a0b2
Agustin Linari <ALINARI@gire.com>
2019-03-04 17:20:17 -0300

    Configuracion de mongodb para que no use el embebido en perfil dev

--- evaluations/src/main/java/com/gire/eval360/evaluations/configuration/MongoConfiguration.java
+++ evaluations/src/main/java/com/gire/eval360/evaluations/configuration/MongoConfiguration.java
@@ -2,6 +2,7 @@ package com.gire.eval360.evaluations.configuration;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractReactiveMongoConfiguration;
 import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 
