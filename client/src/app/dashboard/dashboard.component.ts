ef19e379b73bbc77d20b13fcdb889520d0d526d0
Agustin Linari <ALINARI@gire.com>
2019-04-07 22:26:05 -0300

    detalle

--- client/src/app/dashboard/dashboard.component.ts
+++ client/src/app/dashboard/dashboard.component.ts
@@ -9,7 +9,7 @@ import { Router } from '@angular/router';
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css'],
 })
-export class DashboardComponent implements OnInit{
+export class DashboardComponent implements OnInit {
 
   private roles: String[];
 
