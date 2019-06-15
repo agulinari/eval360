fab94f1891cca20052d92ca3ac242e8823c50b80
Agustin Linari <ALINARI@gire.com>
2019-06-15 12:17:24 -0300

    se cambia el logo en el mail

--- notifications/src/main/java/com/gire/eval360/notifications/service/EmailServiceImpl.java
+++ notifications/src/main/java/com/gire/eval360/notifications/service/EmailServiceImpl.java
@@ -24,7 +24,7 @@ public class EmailServiceImpl implements EmailService {
     	MimeMessage message = emailSender.createMimeMessage();
         MimeMessageHelper helper = new MimeMessageHelper(message,MimeMessageHelper.MULTIPART_MODE_MIXED_RELATED,StandardCharsets.UTF_8.name());
         helper.setText(html,true);
-        helper.addInline("logo",new ClassPathResource("static/imgs/personas.png"));
+        helper.addInline("logo",new ClassPathResource("static/imgs/logo.png"));
         helper.addInline("css", new ClassPathResource("static/css/style.css"));
         message.setFrom(new InternetAddress("no-reply@eval360.com", "NoReply"));
         message.setReplyTo(InternetAddress.parse("no-reply@eval360.com", false));
